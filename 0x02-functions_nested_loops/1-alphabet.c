61fb47f452e5480fee4e21b231543b8fc215a4ea
Emiliaverse <emiliatech24@gmail.com>
2022-04-12 08:46:42 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * print alphabet - print alphabet lowercase
+ * print_alphabet - print alphabet lowercase
  *
- * Return: Always 0
- */
+ **/
 
 void print_alphabet(void)
 {
 	char alphabet;
-
+	
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		_putchar(alphabet);
