8c7a1aa9220005bc30cd5dafe71f8fe80b3ba9b1
Emiliaverse <emiliatech24@gmail.com>
2022-04-12 21:58:07 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,6 +25,12 @@ void print_times_table(int n)
 				      _putchar('0' + rep / 10);
 				      _putchar('0' + rep % 10);
 			     }
+			     else if (rep < 100)
+			     {
+				     _putchar(' ');
+				     _putchar('0' + rep / 10);
+				     _putchar('0' + rep % 10);
+			     }
 			     else
 			     {
 				     _putchar('0' + rep / 100);
