ecfa718dd7b4a69e5aa011b10345259bf46e9503
Emiliaverse <emiliatech24@gmail.com>
2022-04-24 00:15:25 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,7 +4,6 @@
  * @str: the string to change the first letter of a word in uppercase
  * Return: capitalizes letters
  */
-
 char *cap_string(char *str)
 {
 	int i;
@@ -12,11 +11,10 @@ char *cap_string(char *str)
 	char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 
 	i = 0;
-	
+
 	while (str[i] != '\0')
 	{
 		if (i == 0 && str[i] >= 97 && str[i] <= 122)
-
 		{
 			str[i] = str[i] - 32;
 		}
